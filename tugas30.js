61a2a983347b65cdbc784c7c5a4f44098afbc76e
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 17:11:12 +0700

    Tugas 30 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas30.js
@@ -0,0 +1,5 @@
+function regex(value) {
+   let comotData = value.match(/[es]/g);
+   console.log(comotData);
+}
+regex("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
